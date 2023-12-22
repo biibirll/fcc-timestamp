b13d09fc136be90d15894a0ce7af1d666dfe5393
biibirll <gabriel.bonagio16@gmail.com>
2023-12-22 15:17:30 -0300

    fix: correctly parse unix timestamps

--- index.ts
+++ index.ts
@@ -16,8 +16,15 @@ app.get("/api/", (req, res) => {
   res.json({ unix: currentTime.getTime(), utc: currentTime.toUTCString() });
 });
 
-app.get("/api/:timestamp", (req, res) => {
-  const date = new Date(req.params.timestamp);
+app.get("/api/:date", (req, res) => {
+  const onlyDigitsRE = /^[0-9]+$/;
+  let date: Date;
+
+  if (onlyDigitsRE.test(req.params.date)) {
+    date = new Date(parseInt(req.params.date));
+  } else {
+    date = new Date(req.params.date);
+  }
 
   if (isNaN(date.getTime())) {
     return res.status(400).json({ error: "Invalid Date" });
